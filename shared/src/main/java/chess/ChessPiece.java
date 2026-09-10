3895245abc62d244619718853312520f02526ded
throc <ethrockm@byu.edu>
2026-09-10 14:28:57 -0600

    Added functionality for the getter of piece type getPieceType().

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -40,7 +40,7 @@ public class ChessPiece {
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return typeG;
     }
 
     /**
